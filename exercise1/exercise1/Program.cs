eb70160b0fd7143e4a94d642ecc6ecd8d0d4c630
pawa <>
2018-10-15 16:18:38 +0300

    exercise in c sharp. reload with modifications

--- exercise1/exercise1/Program.cs
+++ exercise1/exercise1/Program.cs
@@ -4,27 +4,32 @@ namespace exercise1
 {
     class Program
     {
+
+        static void formula(double i, double a, double b)
+        {
+            double y = Math.Log10(Math.Pow(i, 2) - 1) / Math.Log(a * Math.Pow(i, 2) - b, 5);
+            Console.WriteLine($"Для x = {i}, y = {Math.Round(y, 4)}");
+        }
+
         static void Main(string[] args)
         {
-            double y;
             double a = 1.1;
             double b = 0.09;
             Console.WriteLine("9 вариант");
+            Console.WriteLine();
             Console.WriteLine("Задание 1");
             for (double i = 1.2; i <= 2.2; i = i + 0.2)
             {
-                y = Math.Log10(Math.Pow(i, 2) - 1) / Math.Log(a * Math.Pow(i, 2) - b,5);
-                Console.WriteLine($"Для шага {i} функция y = {Math.Round(y, 4)}");
+                formula(i, a, b);
             }
 
             Console.WriteLine();
             Console.WriteLine("Задание 2");
 
             double[] mass = new double[5] { 1.21, 1.76, 2.53, 3.48, 4.52 };
-            for (int j = 0; j < mass.Length; j++)
+            for (int i = 0; i < mass.Length; i++)
             {
-                y = Math.Log10(Math.Pow(mass[j], 2) - 1) / Math.Log(a * Math.Pow(mass[j], 2) - b,5);
-                Console.WriteLine($"Для шага {mass[j]} функция y = {Math.Round(y, 4)}");
+                formula(mass[i], a, b);
             }
 
             Console.ReadKey();
