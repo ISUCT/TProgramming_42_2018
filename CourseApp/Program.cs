d1af50e7d5af5d2e4b7c2a7468d818f608325c45
Danila <danchik2255@gmail.com>
2018-10-16 12:03:32 +0300

    Added solver for task 2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using static Solver;
+using static IO;
 
 namespace CourseApp
 {
@@ -6,7 +8,15 @@ namespace CourseApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Гусев Данила Михайлович");
+            IO.writeString("Task 1");
+            Dictionary<string, double> data = IO.read();
+            Dictionary<string, double> answers = Solver.solve(data["a"], data["b"], data["xH"], data["xK"], data["dX"]);
+            IO.write(answers);
+
+            IO.writeString("Task 2");
+            double[] dataArr = IO.readArray();
+            answers = Solver.solveXArray(data["a"], data["b"], dataArr);
+            IO.write(answers);
         }
     }
 }
