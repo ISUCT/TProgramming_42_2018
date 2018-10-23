7ec1a52cfc5bad83c5ffe87f8a01e40c1d14d9c7
User of DK12 <DK12@it.isuct.ru>
2018-10-23 11:57:01 +0400

    Laba2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CourseApp
+namespace Function_Lifanov2_42
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Лифанов Олег ");
-            Console.ReadLine();
+            double a = 2.5;
+            double b = 4.6;
+
+            // ЗАДАНИЕ А
+
+            Console.WriteLine("ЗАДАНИЕ А");
+            for (double x = 1.1; x <= 3.6; x += 0.5)
+            {
+                Console.WriteLine($"y({x})={Func(x, a, b)}");
+            }
+
+            // ЗАДАНИЕ B
+
+            Console.WriteLine("ЗАДАНИЕ B");
+            double[] mas = new double[5] { 1.2, 1.28, 1.36, 1.46, 2.35 };
+            foreach (double x in mas)
+            {
+                Console.WriteLine($"y({x})={Func(x, a, b)}");
+            }
+            Console.ReadKey();
+        }
+        static double Func(double x, double a, double b)
+        {
+            return ((a + b * x) * (a + b * x) * ((a + b * x) / 2)) / (1 + Math.Log10(a + b * x));
         }
     }
 }
