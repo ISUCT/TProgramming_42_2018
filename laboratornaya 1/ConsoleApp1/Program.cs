a9181b31305d0ded6bc0264fbfe496a6edfb4b79
User of DK12 <DK12@it.isuct.ru>
2018-10-29 13:30:32 +0400

    3raz

---
+++ laboratornaya 1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Formula(double x)
+        {
+            double y = (Math.Pow(Math.Sin(x), 3) + Math.Pow(Math.Cos(x), 3)) * Math.Log(x);
+            Console.WriteLine($"При начальном и конечном х = {x} функция y = {Math.Round(y, 4)}");
+        }
+        static void Main()
+        {
+            Console.WriteLine("1 вариант");
+            for (double x = 0.11; x <= 0.36; x = x + 0.05)
+            {
+                Formula(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine("2 вариант");
+            double[] z = new double[5] { 0.2, 0.3, 0.38, 0.43, 0.57 };
+            for (int i = 0; i <= 4; i++)
+            {
+                Formula(z[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
