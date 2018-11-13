2a15902bbe04c4b31bf765065b9561facfb05bfa
Your Name <you@example.com>
2018-11-13 12:59:34 +0400

    Laba-4.A

---
+++ CourseApp/Pig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Pig
+    {
+        private int salo;
+        private int age;
+        private string pol;
+     
+        private Pig()
+        {
+            pol = "M";
+            age = 20;
+            salo = 70;
+        }
+        private Pig(string n )
+        {
+            pol = n;
+            if (pol == "M")
+            {
+                age = 15; salo = 45;
+            }
+            else
+            { age = 20; salo = 70; }
+        }
+
+    }
+
+}
