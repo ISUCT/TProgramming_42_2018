0f421ccbb19821fb7674fb5fe2ed7b1eb12d14e7
User of DK12 <DK12@it.isuct.ru>
2018-11-27 13:00:55 +0400

    tretya laba test

--- CourseApp.Tests/UnitTest2.cs
+++ CourseApp.Tests/UnitTest2.cs
@@ -9,6 +9,18 @@ namespace CourseApp.Tests
         [Fact]
         public void Test12()
         {
-         
+            Pistol Colt = new Pistol();
+            var name = Colt.name;
+            var kalibr = Colt.kalibr;
+            var emkostmag = Colt.emkostmag;
+            var mass = Colt.mass;
+            Assert.Equal("Colt", name);
+            Assert.Equal(0.45, kalibr);
+            Assert.Equal(7, emkostmag);
+            Assert.Equal(1.4f, mass);
+           
         }
-   
+
+
+    }
+}
--- CourseApp/Pistol.cs
+++ CourseApp/Pistol.cs
@@ -7,8 +7,8 @@ namespace Zadanie1
         public double kalibr;
         public float mass;
         public int emkostmag;
-
-        public void GetInfo()
+        public Pistol() { name = "Colt";kalibr = 0.45;mass = 1.4f;emkostmag = 7; }
+       public void GetInfo()
         {
             Console.WriteLine($"Наименование: {name}  Калибр: {kalibr} Масса: {mass} Емкость магазина: {emkostmag}");
         }
