02ed967c3f8f55e03308ce2a4758b2c63ed0c304
Viktor_Fedorov <vitte.fedorow@yandex.ru>
2018-12-31 01:00:49 +0300

    LLLL

---
+++ CourseApp/Pistol.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Zadanie1
+{
+    public class Pistol : Gun
+    {
+        public Pistol()
+        {
+            Name = "Colt";
+            Kalibr = 0.45;
+            Mass = 1.4f;
+            Emkostmag = 7;
+        }
+
+        public Pistol(string name, double kalibr, float mass )
+            : base(name, kalibr, mass)
+        {
+            Emkostmag = 7;
+        }
+
+        public override string ToString()
+        {
+            return $"Наименование: {Name}  Калибр: {Kalibr} Масса: {Mass} Емкость магазина: {Emkostmag}\n Пистолет {Name} выстрелил: \"Бах!\"";
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine(Display());
+        }
+
+        public override string Display()
+        {
+            return @"
+
+      +-'~`---------------------------------/\--
+ ||"""""""""""""""""""""""""""""""" \\\\\\  \/~)
+ ||                                  \\\\\\  \/_
+  |~~~~~~~~-________________-_________________\ ~--_
+  !---------|_________       ------~~~~~(--   )--~~
+                      \ /~~~~\~~\   )--- \_ /(
+                       ||     |  | \   ()   \\
+                       \\____/_ / ()\        \\
+                        `~~~~~~~~~-. \        \\
+                                    \ \  <($)> \\
+                                     \ \        \\
+                                      \ \        \\
+                                       \ \        \\
+                                        \ \  ()    \|
+                                        _\_\__====~~~
+       
+
+";
+        }
+    }
+}
