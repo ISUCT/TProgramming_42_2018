bbab0793e364ba7afb947460811279d64b182396
Oleg Lifanov <lifanov.o.1999@mail.ru>
2019-01-03 13:22:38 +0300

    Modified Ship.cs and added UnitTest2

--- CourseApp/Ship.cs
+++ CourseApp/Ship.cs
@@ -33,6 +33,69 @@ namespace CourseApp
             vodoizmeschenie = c;
         }
 
+        public int Passangers
+        {
+            get
+            {
+                return passangers;
+            }
+
+            set
+            {
+                if (value > 250 || value < 0)
+                {
+                    Console.WriteLine("Невозможное количество пассажиров(min:0 max:250)");
+                    throw new Exception();
+                }
+                else
+                {
+                    passangers = value;
+                }
+            }
+        }
+
+        public int Dlina
+        {
+            get
+            {
+                return dlina;
+            }
+
+            set
+            {
+                if (value > 1000 || value < 5)
+                {
+                    Console.WriteLine("Невозможная длина корабля(min:5 max:1000)");
+                    throw new Exception();
+                }
+                else
+                {
+                    dlina = value;
+                }
+            }
+        }
+
+        public int Vodoizmeschenie
+        {
+            get
+            {
+                return vodoizmeschenie;
+            }
+
+            set
+            {
+                if (value > 2000 || value < 10)
+                {
+                    Console.WriteLine("Невозможное водоизмещение корабля(min:10 max:2000)");
+                    throw new Exception();
+                }
+                else
+                {
+                    vodoizmeschenie = value;
+                }
+            }
+        }
+
         public void Info()
         {
             Console.WriteLine($" пассажиров: {passangers} длина: {dlina} водоизмещение: {vodoizmeschenie}");
