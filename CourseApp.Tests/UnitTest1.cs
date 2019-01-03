f46561dcba164fa82f08b97d6fcc1d8687e7380f
Oleg Lifanov <lifanov.o.1999@mail.ru>
2019-01-03 11:07:27 +0300

    Just a lil update

--- CourseApp.Tests/UnitTest1.cs
+++ CourseApp.Tests/UnitTest1.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using CourseApp;
 
-namespace CourseApp.Test
+namespace CourseApp.Tests
 {
     public class UnitTest1
     {
