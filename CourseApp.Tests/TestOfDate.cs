5ce6c9839fe8cca378dba0f85af2a78c1932c11f
User of A309 <a309@it.isuct.ru>
2019-03-06 14:09:23 +0400

    added to day

--- CourseApp.Tests/TestOfDate.cs
+++ CourseApp.Tests/TestOfDate.cs
@@ -9,9 +9,7 @@ namespace CourseApp.Tests
         [Fact]
         public void DateIsNotBelowZeroAndBelowHundredYear()
         {
-        DateTime value1 = new DateTime(1999, 10, 26);
-        DateTime value2 = new DateTime(2019, 03, 05);
-        DateTime date = Date.Calc(value1, value2);
+        DateTime date = Date.Calc(new DateTime(1999, 10, 26), new DateTime(2019, 03, 05));
         DateTime min = new DateTime(1, 1, 12);
         DateTime max = new DateTime(100, 12, 31);
         Assert.InRange(date, min, max);
