71b73340dcf9a05e781bc68f3e9ad6448bc20d27
pawa <>
2019-04-23 02:11:19 +0300

    exam exercises
    exercises numbers 11, 14, 16

---
+++ exercise 11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercise_11
+{
+    public class Program
+    {
+        public static int Summa(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static void Main()
+        {
+            int[] Massiv = new int[10];
+            Massiv[0] = 4;
+            Massiv[1] = 2;
+            Massiv[2] = 7;
+
+            for (int i = 3; i < 10; i++)
+            {
+                Massiv[i] = Summa(Massiv[i - 3], Massiv[i - 2], Massiv[i - 1]);
+            }
+
+            Console.Write("Первые 10 элементов ряда: ");
+
+            foreach (int j in Massiv)
+            {
+                Console.Write($"{j} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercise 14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercise_14
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            double howlong = 200;
+            Console.WriteLine(car.start());
+            Console.WriteLine($"За время равное {howlong} минутам, расстроение пройденное машиной будет равно {car.drive(200)}");
+            Console.WriteLine(car.stop());
+            Console.ReadKey();
+        }
+    }
+}
